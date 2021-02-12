96ee1a38f7267e5a188dfecf83b8907fbfcbc534
Maggie S <Maggiesaldivia@gmail.com>
2021-02-12 12:55:59 -0800

    add UI functions and fix spacing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,35 @@
-
 //business logic
 function beepBoop(num) {
   let arr = [];
   let finalArr=[];
   for (let i=0; i<=num; i++) {
-    arr.push(" "+ i);
+    arr.push(i);
   }
 	for (let i in arr) {
 		if (i.includes(3)) {
     finalArr.push("Won't you be my neighbor?")
     } else if (i.includes(2)) {
-    finalArr.push("Boop!")
+      finalArr.push("Boop!")
     } else if (i.includes(1)) {
-    finalArr.push("Beep!")
-    } else{
-    finalArr.push(i)
+      finalArr.push("Beep!")
+    } else {
+      finalArr.push(i)
     }
 	}
-	console.log(finalArr)
-  
-  }
-  
-  beepBoop(13)
+  return finalArr
+}
+
+$(document).ready(function() {
+  $("form.input").submit(function(event) {
+    event.preventDefault();
+
+    const userNum =("$input.quantity").val();
+    const robogerOutput = (beepBoop(userNum));
+
+    $("#output").text(robogerOutput);
+
+    $("form#input").hide();
+    $("#output").show();
+
+  })
+})
