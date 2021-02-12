0201101e1e398bad96aaa2e5f4b8b134691a1ff8
Maggie S <Maggiesaldivia@gmail.com>
2021-02-11 22:31:26 -0800

    add new for loops and arrays to pass tests 3 and 5

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,20 @@
 
 function beepBoop(num) {
   let arr = [];
-  let transformedArr = []
+  let finalArr=[];
   for (let i=0; i<=num; i++) {
     arr.push(" "+ i);
   }
-  console.log(arr)
+  //console.log(arr)
+	for (let i in arr) {
+		if (i.includes(3)) {
+    finalArr.push("won't you be my neighbor")
+    } else {
+    finalArr.push(i)
+    }
+	}
+	console.log(finalArr)
   
   }
+  
+  beepBoop(13)
